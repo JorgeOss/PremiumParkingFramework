9c72b0c4095ff5a68c90a07fba5d1aa5f819da1a
JorgeOss <jorge788oss@gmail.com>
2023-02-14 21:48:49 -0600

    new update

--- src/main/java/webPages/DashboardPage.java
+++ src/main/java/webPages/DashboardPage.java
@@ -1,8 +1,5 @@
 package webPages;
 
-import java.util.List;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -19,10 +16,6 @@ public class DashboardPage {
 	}
 	// storing the dashboard's web elements
 
-	String month = "March 2022";
-	String day = "14";
-	
-	
 	@FindBy(how = How.PARTIAL_LINK_TEXT, using = "Validations")
 	WebElement Validations;
 
@@ -34,13 +27,13 @@ public class DashboardPage {
 
 	@FindBy(how = How.ID, using = "validation_form_end_time")
 	WebElement EndDate;
-	
+
 	@FindBy(how = How.XPATH, using = "(//button[@type='button'])[9]")
 	WebElement NextMonth;
-	
+
 	@FindBy(how = How.XPATH, using = "/html/body/div[5]/div[1]/div[2]/table/tbody/tr[4]/td[4]/div")
 	WebElement PickADate;
-	
+
 	@FindBy(how = How.ID, using = "validation_form_customer_name")
 	WebElement CustomerName;
 
@@ -49,10 +42,9 @@ public class DashboardPage {
 
 	@FindBy(how = How.ID, using = "validation_form_notes")
 	WebElement Notes;
-	
+
 	@FindBy(how = How.CLASS_NAME, using = "dropdown-link")
 	WebElement clickSignOut;
-	
 
 	@FindBy(how = How.XPATH, using = "//a[@href='/profile/signout']")
 	WebElement SignOut;
@@ -76,9 +68,7 @@ public class DashboardPage {
 		NextMonth.click();
 		PickADate.click();
 		Thread.sleep(1000);
-		
-		
-		
+
 	}
 
 	public void customerName() {
@@ -93,15 +83,15 @@ public class DashboardPage {
 		Notes.sendKeys("The customer will stay for 7 hours and will let us know if decides to extend the license plate validation to overnight, any questions call to her room number 1012");
 		Thread.sleep(3000);
 	}
-	
+
 	public void clickProfile() {
 		clickSignOut.click();
 	}
 
 	public void logOut() {
-		WebDriverWait wait =  new WebDriverWait(driver, 20);
+		WebDriverWait wait = new WebDriverWait(driver, 20);
 		wait.until(ExpectedConditions.elementToBeClickable(SignOut));
 		SignOut.click();
-		
+
 	}
 }
